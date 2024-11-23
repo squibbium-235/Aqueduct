8828a6f5ef956dc05ef7598e6ea217a5f6cf2686
Will Squibb <109985186+gr0wnd-z33r0@users.noreply.github.com>
2024-11-23 22:34:16 +0000

    Update main_ui.py
    finished off RNG features and allower the user to return back to the mainmenu and quit the program

--- src/ui/main_ui.py
+++ src/ui/main_ui.py
@@ -2,19 +2,19 @@ import random
 
 while True:
     print("Main Menu")
-    print("1. Random Number Generation")
-    print("2. Converters")
-    print("3. Quit")
+    print("(1)      Random Number Generation")
+    print("(2)      Converters")
+    print("(3)      Quit")
     choice = input("> ")
 
     if choice == '1':
         while True:
-            print("Random Number Generators")
-            print("1. Random Integer")
-            print("2. Random Decimal")
-            print("3. Coin Flip")
-            print("4. Random Percentage")
-            print("5. Back")
+            print("Random Number Generators\n")
+            print("(1)      Random Integer")
+            print("(2)      Random Decimal")
+            print("(3)      Coin Flip")
+            print("(4)      Random Percentage")
+            print("(5)      Back")
             choice = input("> ")
 
             if choice == '1':
@@ -22,20 +22,36 @@ while True:
                 number2 = int(input("Enter the higher number: "))
                 print(random.randint(number1,number2))
                 print("")
-            """elif choice == '2':
-                # ... (code for random decimal generation)
+            elif choice == '2':
+                number1 = float(input("Enter the lower decimal number: "))
+                number2 = float(input("Enter the higher decimal number: "))
+                print(random.uniform(number1,number2))
+                print("")
             elif choice == '3':
-                # ... (code for coin flip)
+                number1=random.randint(0,1)
+                if number1==0:
+                    print("Heads!\n")
+                elif number1==1:
+                    print("Tails!\n")
+                else:
+                    print("something has gone horribly fucking wrong...\n")
             elif choice == '4':
-                # ... (code for random percentage)
+                print(str(random.randint(0,100))+"%")
             elif choice == '5':
                 break  # Go back to the main menu
             else:
                 print("Invalid choice. Please try again.")
 
     elif choice == '2':
-        # ... (code for converters)
+        while True:
+            print("(1)      Distance")
+            print("(2)      Mass")
+            print("(3)      Temperature")
+            print("(4)      Volume")
+            print("(5)      Time")
+            print("(6)      Area")
+            print("(7)      Back")
     elif choice == '3':
         break  # Quit the program
     else:
-        print("Invalid choice. Please try again.")"""
+        print("Invalid choice. Please try again.")
